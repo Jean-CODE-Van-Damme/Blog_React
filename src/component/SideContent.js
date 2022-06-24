ecef10ceba1be1277ea82b4cfd676ad9df05a50f
Jean-CODE-Van-Damme <oreverseau@live.fr>
2022-06-25 00:54:45 +0200

    jedi vs

--- src/component/SideContent.js
+++ src/component/SideContent.js
@@ -5,7 +5,7 @@ const SideContent = (props) => {
     <>
       <div className="about__me">
         <AboutMe
-          first_title={"Eclater Dark-Vador au combat de pouce"}
+          first_title={"Eclater Dark-Vador au combat de poucec"}
           description={
             "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout."
           }
