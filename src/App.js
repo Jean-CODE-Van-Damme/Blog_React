a378710169f42ab89cb64c2c500f3595d2d14820
Jean-CODE-Van-Damme <oreverseau@live.fr>
2022-06-29 13:55:35 +0200

    body ok

--- src/App.js
+++ src/App.js
@@ -7,17 +7,17 @@ import Footer from "./component/Footer";
 const App = () => {
   return (
     <>
-      <div className="body">
-        <div className="container">
-          <Header
-            title={"Yoda Blog"}
-            baseline={"Comment devenir un Jedi en 3 Etapes"}
-          />
-          <NavigationMenu link={"Link"} />
-          <Content />
-          <Footer end={"Made with React at Le reacteur By Olivier Reverseau"} />
-        </div>
+      {/* <div className="body"> */}
+      <div className="container">
+        <Header
+          title={"Yoda Blog"}
+          baseline={"Comment devenir un Jedi en 3 Etapes"}
+        />
+        <NavigationMenu link={"Link"} />
+        <Content />
+        <Footer end={"Made with React at Le reacteur By Olivier Reverseau"} />
       </div>
+      {/* </div> */}
     </>
   );
 };
